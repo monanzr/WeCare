7cab074aed738adbe016655be0b517b313ff2b28
Mona Nazari <monanazari8@gmail.com>
2022-06-12 16:19:42 +0430

    Add files via upload
    add responsive menu and edit responsive bugs

--- js/index.js
+++ js/index.js
@@ -55,23 +55,14 @@ $(document).ready(function() {
 });
 
 
-// var card7 = document.querySelector(".card--7");
-//
-// card7.addEventListener('mousemove', function (e) {
-//
-//     var wh = window.innerHeight / 2,
-//         ww = window.innerWidth / 2;
-//     card7.style.setProperty('--mouseX7', (e.clientX - ww) / 25);
-//     card7.style.setProperty('--mouseY7', (e.clientY - wh) / 25);
-//
-// });
-//
-// card7.addEventListener('mouseleave', function (e) {
-//
-//     card7.style.setProperty('--mouseX7', 0);
-//     card7.style.setProperty('--mouseY7', 0);
-//
-// });
+function openNav() {
+    document.getElementById("navSide").style.width = "min(100%, 23rem)";
+}
+
+function closeNav() {
+    document.getElementById("navSide").style.width = "0";
+}
+
 
 function openTab(evt, tab) {
     var i, tabcontent, tablinks;
